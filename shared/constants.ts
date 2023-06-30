8b920ed3d06a7c843f73c22b71d7412400034a5f
Piotr Dyrda <53171952+dyrpit@users.noreply.github.com>
2023-06-30 21:18:35 +0200

    Feat/be streamers api route (#1)

---
+++ shared/constants.ts
@@ -0,0 +1,8 @@
+export const API_ROUTES = {
+  STREAMERS: '/streamers',
+};
+
+export const VOTE_TYPES = {
+  UPVOTE: 'upvote',
+  DOWNVOTE: 'downvote',
+};
---
+++ shared/interfaces.ts
@@ -0,0 +1,11 @@
+import { VoteType } from './types';
+
+export interface CreateStreamerDto {
+  name: string;
+  description: string;
+  platforms: string[];
+}
+
+export interface VoteDto {
+  voteType: VoteType;
+}
