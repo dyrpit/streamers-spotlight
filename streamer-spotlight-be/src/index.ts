c98e7d32bf4cb8797b1cc4e357b2a4d40d7c487d
Piotr Dyrda <piotr.dyrda88@gmail.com>
2023-07-01 22:13:29 +0200

    add cors to BE

--- streamer-spotlight-be/src/index.ts
+++ streamer-spotlight-be/src/index.ts
@@ -1,11 +1,13 @@
 import { Response, Request } from 'express';
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 import streamersRoute from './routes/streamer-routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(streamersRoute);
 
