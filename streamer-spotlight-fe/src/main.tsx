8fb516b3de9a8ebbe5427c412bf3252c88ef7c2b
Piotr Dyrda <piotr.dyrda88@gmail.com>
2023-07-02 11:26:39 +0200

    add react router

--- streamer-spotlight-fe/src/main.tsx
+++ streamer-spotlight-fe/src/main.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.tsx';
+import StreamersView from './views/StreamersView.tsx';
 
 import { queryClient } from './constants/query-client.ts';
 
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        path: '/',
+        element: <StreamersView />,
+      },
+      {
+        path: '/:streamerId',
+        element: <div>Streamer details view</div>,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
